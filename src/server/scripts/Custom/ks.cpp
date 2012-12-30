1e70f9b4ff2d27b4d2097370b4e082389aaa2f87
ikizami <shadowzxi@live.co.uk>
2012-12-30 11:47:09 +0000

    Signed-off-by: ikizami <shadowzxi@live.co.uk>

--- src/server/scripts/Custom/ks.cpp
+++ src/server/scripts/Custom/ks.cpp
@@ -1,54 +1,43 @@
 class KillStreak_System : public PlayerScript
-
 {
+				public:
+								KillStreak_System() : PlayerScript("KillStreak_System"){}
+							  
+								struct KillStreak_Info
+								{
+												uint32 killstreak;
+												uint64 lastkill;
+								};
+								static std::map<uint64, KillStreak_Info> KillStreakData;
+							  
+								void OnPvPKill(Player * Killer, Player * Victim)
+								{
+												uint64 KillerGUID = Killer->GetGUID();
+												uint64 VictimGUID = Victim->GetGUID();
+											  
+												if (KillerGUID == VictimGUID || KillStreakData[KillerGUID].lastkill == VictimGUID)
+																return;
+															  
+												KillStreakData[KillerGUID].killstreak++;
+												KillStreakData[KillerGUID].lastkill = VictimGUID;
+												KillStreakData[VictimGUID].killstreak = 0;
+												KillStreakData[VictimGUID].lastkill = 0;
+												char message[250];
+												switch( KillStreakData[KillerGUID].killstreak )
+												{
+																case 1:
+																				snprintf(message, 250, "[PVP System]: %s is on a killstreak of %u!", Killer->GetName(), KillStreakData[KillerGUID].killstreak);
+																				break;
+																case 10:
+																				snprintf(message, 250, "[PVP System]: %s is on a killstreak of %u!", Killer->GetName(), KillStreakData[KillerGUID].killstreak);
 
-		public:
-				KillStreak_System() : PlayerScript("KillStreak_System"){}				
-
-				struct KillStreak_Info
-
-				{
-						uint32 killstreak;
-						uint64 lastkill;
-
-				};
-
-				static std::map<uint64, KillStreak_Info> KillStreakData;				
-
-				void OnPvPKill(Player * Killer, Player * Victim)
-				{
-						uint64 KillerGUID = Killer->GetGUID();
-						uint64 VictimGUID = Victim->GetGUID();						
-
-						if (KillerGUID == VictimGUID || KillStreakData[KillerGUID].lastkill == VictimGUID)
-
-								return;
-								
-						KillStreakData[KillerGUID].killstreak++;
-						KillStreakData[KillerGUID].lastkill = VictimGUID;
-						KillStreakData[VictimGUID].killstreak = 0;
-						KillStreakData[VictimGUID].lastkill = 0;
-						char message[250];
-
-						switch( KillStreakData[KillerGUID].killstreak )
-						{
-								case 1:
-										snprintf(message, 250, "[PVP System]: %s is on a killstreak of %u!", Killer->GetName(), KillStreakData[KillerGUID].killstreak);
-										break;
-								case 5:
-										snprintf(message, 250, "[PVP System]: %s is on a killstreak of %u!", Killer->GetName(), KillStreakData[KillerGUID].killstreak);
-										break;
-								case 10:
-										snprintf(message, 250, "[PVP System]: %s is on a killstreak of %u!", Killer->GetName(), KillStreakData[KillerGUID].killstreak);
-										break;
-						}
-						 sWorld->SendServerMessage(SERVER_MSG_STRING, message, 0);
-				}
+																				break;
+												}
+												 sWorld->SendServerMessage(SERVER_MSG_STRING, message, 0);
+								}
 };
-
- 
 
 void AddSC_KillStreak_System()
 {
-		new KillStreak_System;
+				new KillStreak_System;
 }
